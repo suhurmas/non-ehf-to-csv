4673dafb61749ce818a34786f70990a9391d95f4
Mats Jørgensen <matsjorgensen@outlook.com>
2019-01-21 06:15:09 +0100

    Added check for table without non_ehf_users, and a sql statement to create them.

--- nonehftocsv.py
+++ nonehftocsv.py
@@ -28,6 +28,30 @@ df.drop_duplicates(subset ="identifier",
                      keep = False, inplace = True)
 df[["identifier", "name", "EHF_CREDITNOTE_2_0"]].to_sql("elma", conn, if_exists='append', index=False) # keep only "identifier", "name", "EHF_CREDITNOTE_2_0"
 
+# Check if there is any non_ehf users at all. If not; create some
+c.execute('SELECT count(*) FROM elma WHERE EHF_CREDITNOTE_2_0="Nei"')
+has_non_ehf_users = c.fetchone()
+if(has_non_ehf_users[0] > 0):
+    print("DB has no non_ehf_users, creating some")
+    print(has_non_ehf_users[0])
+    c.execute('''
+    UPDATE elma
+    SET EHF_CREDITNOTE_2_0="Nei"
+    WHERE identifier
+    IN
+    (SELECT identifier
+    FROM elma
+    ORDER BY RANDOM()
+    LIMIT 100)
+    ''')
+    c.execute('SELECT count(*) FROM elma WHERE EHF_CREDITNOTE_2_0="Nei"')
+    has_non_ehf_users = c.fetchone()
+    print(has_non_ehf_users[0])
+    conn.commit()
+
+quit()
+
+
 # Select 100 first entries of ELMA users without EHF
 c.execute('SELECT identifier, name FROM elma WHERE EHF_CREDITNOTE_2_0="Nei" LIMIT 100')
 elma_ehf_no = c.fetchall()
