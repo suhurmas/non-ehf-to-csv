a2e4f24570b787cea8d840e771bdbfe41eebad8f
Mats Jørgensen <matsjorgensen@outlook.com>
2019-01-21 06:57:40 +0100

    Fixed some indenation

--- nonehftocsv.py
+++ nonehftocsv.py
@@ -3,6 +3,7 @@ import sqlite3
 import pandas
 from io import StringIO
 
+pandas.options.mode.chained_assignment = None  # Itterating and reusing creates warnings with pandas, this suppresses those warnings
 # Initialize DB
 db = "ELMA-EHF.db"
 conn = sqlite3.connect(db)
@@ -20,29 +21,26 @@ difi_result = requests.get('http://hotell.difi.no/download/difi/elma/participant
 print("Difi ELMA participants downloaded successfully")
 
 # Write CSV file to DB
-# Insert rows in csv file using pandas lib
 df = pandas.read_csv(StringIO(difi_result.text), delimiter=";")
 # Dropping duplicte values, since difi_result contains something like 40K duplicates
 df.sort_values("identifier", inplace = True)
-df.drop_duplicates(subset ="identifier",
-                     keep = False, inplace = True)
-df[["identifier", "name", "EHF_CREDITNOTE_2_0"]].to_sql("elma", conn, if_exists='append', index=False) # keep only "identifier", "name", "EHF_CREDITNOTE_2_0"
+df.drop_duplicates(subset ="identifier", keep = False, inplace = True)
+df = df[["identifier", "name", "EHF_CREDITNOTE_2_0"]] # keep only "identifier", "name", "EHF_CREDITNOTE_2_0"
+df.to_sql("elma", conn, if_exists='append', index=False)
 
 # Check if there is any non_ehf users at all. If not; create some
 c.execute('SELECT count(*) FROM elma WHERE EHF_CREDITNOTE_2_0="Nei"')
 has_non_ehf_users = c.fetchone()
 if(has_non_ehf_users[0] == 0):
     print("DB has no non_ehf_users, creating some")
-    c.execute('''
-    UPDATE elma
-    SET EHF_CREDITNOTE_2_0="Nei"
-    WHERE identifier
-    IN
-    (SELECT identifier
-    FROM elma
-    ORDER BY RANDOM()
-    LIMIT 100)
-    ''')
+    c.execute('''UPDATE elma
+                SET EHF_CREDITNOTE_2_0="Nei"
+                WHERE identifier
+                IN
+                (SELECT identifier
+                FROM elma
+                ORDER BY RANDOM()
+                LIMIT 100)''')
     conn.commit()
 
 # Select 100 first entries of ELMA users without EHF
@@ -67,16 +65,15 @@ print(" %s brreg ELMA participants without EHF downloaded successfully" % len(re
 
 # Read previously fetched CSV, combine them into one pandas dataframe, with only relevant info
 data_frame = pandas.DataFrame()
-pandas.options.mode.chained_assignment = None  # Itterating like this creates warnings with pandas, this suppresses those warnings
 for result in results:
     # Convert Brreg info to pandas data frame
     df = pandas.read_csv(StringIO(result), delimiter=";")
     # Add rows to data_frame
     data_frame = data_frame.append(df)
 # Select which collums to keep
-address_info = data_frame[["organisasjonsnummer","forretningsadresse.adresse","forretningsadresse.postnummer"
-                 ,"forretningsadresse.poststed","forretningsadresse.landkode"
-                 ,"forretningsadresse.land"]]
+address_info = data_frame[["organisasjonsnummer","forretningsadresse.adresse",
+                           "forretningsadresse.postnummer","forretningsadresse.poststed",
+                           "forretningsadresse.landkode" ,"forretningsadresse.land"]]
 # Rename collumns
 address_info.rename(columns=lambda x: x.replace("forretningsadresse.", ""), inplace=True)
 address_info.rename(columns={"organisasjonsnummer":"identifier"}, inplace=True)
@@ -90,23 +87,24 @@ conn = sqlite3.connect(db)
 c = conn.cursor()
 # Write to table "elmaAddress". Bad practice.
 address_info.to_sql("elmaAddress", conn, if_exists='append', index=False)
-# Write to db and close connection
 conn.commit()
+
 # Mark added users as sanitized
 c.execute('''UPDATE elma
-SET sanitized = "yes"
-WHERE identifier IN (SELECT identifier FROM elmaAddress)''')
+            SET sanitized = "yes"
+            WHERE identifier IN (SELECT identifier FROM elmaAddress)''')
 conn.commit()
+
 # Debugging
 result = c.execute('''SELECT * FROM elmaAddress''')
 print("We wrote %s rows from data frame to DB, and DB returned %s rows" % (len(data_frame.index), len(result.fetchall())))
 
 # Get sanitized users and write their identifier, name and address to csv file
 df = pandas.read_sql(sql='''SELECT elma.identifier, name, adresse, postnummer, poststed, landkode, land
-FROM elma
-INNER JOIN elmaAddress
-ON elma.identifier=elmaAddress.identifier
-WHERE elma.sanitized = "yes"''', con=conn)
+                            FROM elma
+                            INNER JOIN elmaAddress
+                            ON elma.identifier=elmaAddress.identifier
+                            WHERE elma.sanitized = "yes"''', con=conn)
 
 print("Constructing csv with %s rows fetched from db" % len(df.index))
 df.to_csv('NonEHFUsers.csv', index=False)
