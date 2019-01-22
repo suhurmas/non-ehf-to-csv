c012962b3b120a5901cd426e2bf8f81782a0684b
Mats Jørgensen <matsjorgensen@outlook.com>
2019-01-22 23:49:31 +0100

    Added html as encoding. json is still default

--- rest.py
+++ rest.py
@@ -26,22 +26,22 @@ def clients():
 
     # Constructing where clauses for the final sql statement
     if(len(postnummer) <= 4 and len(postnummer) > 0):
-        whereclauses.append(" AND postnummer = {0} ".format(postnummer))
+        whereclauses.append(' AND postnummer = {0} '.format(postnummer))
         print(" I see you've tried to fetch the users in area {0}, congratulations".format(postnummer))
     if(len(name) > 0):
-        whereclauses.append("AND name LIKE '{0}''".format(name))
+        whereclauses.append(' AND name LIKE "{0}"'.format(name))
         print(" I see you've tried to fetch the user {0}, congratulations".format(name))
     if(len(adresse) > 0):
-        whereclauses.append(" AND adresse LIKE '{0}' ".format(adresse))
+        whereclauses.append(' AND adresse LIKE "{0}" '.format(adresse))
         print(" I see you've tried to fetch the users at {0}, congratulations".format(adresse))
     if(len(poststed) > 0):
-        whereclauses.append(" AND poststed LIKE '{0}' ".format(poststed))
+        whereclauses.append(' AND poststed LIKE "{0}" '.format(poststed))
         print(" I see you've tried to fetch the users at {0}, congratulations".format(poststed))
     if(len(land) > 0):
-        whereclauses.append(" AND land LIKE '{0}' ".format(land))
+        whereclauses.append(' AND land LIKE "{0}" '.format(land))
         print(" I see you've tried to fetch the users at {0}, congratulations".format(land))
     if(len(landkode) > 0):
-        whereclauses.append(" AND landkode LIKE '{0}' ".format(landkode))
+        whereclauses.append(' AND landkode LIKE "{0}" '.format(landkode))
         print(" I see you've tried to fetch the users at {0}, congratulations".format(landkode))
 
     # Final sql statement
@@ -50,9 +50,13 @@ def clients():
                                 INNER JOIN elmaAddress
                                 ON elma.identifier=elmaAddress.identifier
                                 WHERE elma.sanitized = "yes"{0};'''.format("".join(whereclauses)), con=conn)
+    # Set encoding
     if (encoding.lower() == "csv"):
         df.to_csv(string_, index=False)
         response.content_type = 'text/csv; charset=utf-8'
+    elif (encoding.lower() == "html"):
+            string_ = StringIO((df.to_html()))
+            response.content_type = 'text/html; charset=utf-8'
     else:
         df.to_json(string_, orient="records")
         response.content_type = 'text/json; charset=utf-8'
