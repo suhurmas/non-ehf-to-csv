c2f7b4c168016fb182458ce8c6c2cd8cc0b6de2b
Mats Jørgensen <matsjorgensen@outlook.com>
2019-01-23 01:08:56 +0100

    Added identifier as param input

--- rest.py
+++ rest.py
@@ -10,8 +10,8 @@ db = "ELMA-EHF.db"
 def clients():
     # Connect to db
     conn = sqlite3.connect(db)
-    # Pands expects to write to IO, this is a hack to get the json string
-    string_ = StringIO()
+    # Pandas expects to write to IO, this is a hack to get the response string
+    response_string = StringIO()
     # Parameters we fetch through get params "?=''"
     postnummer = request.query.postnummer
     name = request.query.name
@@ -27,22 +27,18 @@ def clients():
     # Constructing where clauses for the final sql statement
     if(len(postnummer) <= 4 and len(postnummer) > 0):
         whereclauses.append(' AND postnummer = {0} '.format(postnummer))
-        print(" I see you've tried to fetch the users in area {0}, congratulations".format(postnummer))
     if(len(name) > 0):
         whereclauses.append(' AND name LIKE "{0}"'.format(name))
-        print(" I see you've tried to fetch the user {0}, congratulations".format(name))
     if(len(adresse) > 0):
         whereclauses.append(' AND adresse LIKE "{0}" '.format(adresse))
-        print(" I see you've tried to fetch the users at {0}, congratulations".format(adresse))
     if(len(poststed) > 0):
         whereclauses.append(' AND poststed LIKE "{0}" '.format(poststed))
-        print(" I see you've tried to fetch the users at {0}, congratulations".format(poststed))
     if(len(land) > 0):
         whereclauses.append(' AND land LIKE "{0}" '.format(land))
-        print(" I see you've tried to fetch the users at {0}, congratulations".format(land))
     if(len(landkode) > 0):
         whereclauses.append(' AND landkode LIKE "{0}" '.format(landkode))
-        print(" I see you've tried to fetch the users at {0}, congratulations".format(landkode))
+    if(len(identifier) == 9):
+        whereclauses.append(' AND elma.identifier = {0} '.format(identifier))
 
     # Final sql statement
     df = pandas.read_sql(sql='''SELECT elma.identifier, name, adresse, postnummer, poststed, landkode, land
@@ -52,16 +48,16 @@ def clients():
                                 WHERE elma.sanitized = "yes"{0};'''.format("".join(whereclauses)), con=conn)
     # Set encoding
     if (encoding.lower() == "csv"):
-        df.to_csv(string_, index=False)
+        df.to_csv(response_string, index=False)
         response.content_type = 'text/csv; charset=utf-8'
     elif (encoding.lower() == "html"):
-            string_ = StringIO((df.to_html()))
-            response.content_type = 'text/html; charset=utf-8'
+        response_string = StringIO((df.to_html()))
+        response.content_type = 'text/html; charset=utf-8'
     else:
-        df.to_json(string_, orient="records")
+        df.to_json(response_string, orient="records")
         response.content_type = 'text/json; charset=utf-8'
     # Return sql result as encoded string
-    return string_.getvalue()
+    return response_string.getvalue()
 
 #Start server
 run(host='localhost', port=8080)
